747b065180f448aa3abda675862f0ac36646f0c1
Nirmal <nirmalmhnt@gmail.com>
2020-05-21 00:40:27 +0530

    refactor

--- src/main/java/com/nirmal/client/controller/ClientController.java
+++ src/main/java/com/nirmal/client/controller/ClientController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class LuckyWordController {
+public class ClientController {
     @Value("${message}")
     String luckyWord;
 
