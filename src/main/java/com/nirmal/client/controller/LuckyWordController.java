9b15198bbb83f1181fdc6ac1fd3245277633cfb9
Nirmal <nirmalmhnt@gmail.com>
2020-05-21 00:34:47 +0530

    final commit

---
+++ src/main/java/com/nirmal/client/controller/LuckyWordController.java
@@ -0,0 +1,17 @@
+package com.nirmal.client.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LuckyWordController {
+    @Value("${message}")
+    String luckyWord;
+
+    @GetMapping("/lucky-word")
+    public String getLuckyWord(){
+        return luckyWord;
+    }
+
+}
